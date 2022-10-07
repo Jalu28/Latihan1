af8d86ff738bb476fdcde0215f0f610fd8e83362
Jalu28 <115241749+Jalu28@users.noreply.github.com>
2022-10-07 20:21:00 +0700

    Add files via upload

---
+++ MainApp2.java
@@ -0,0 +1,9 @@
+public class MainApp2 {
+    // Ahmad Panjalu R5R 202043501452
+    public static void main(String[] args) {
+        Hewan h = new Hewan();
+
+        h.setEkor("Panjang");
+        System.out.println("Ekornya " + h.getEkor());
+    }
+} 
---
+++ MainApp3.java
@@ -0,0 +1,9 @@
+public class MainApp3 {
+    // Ahmad Panjalu R5R 202043501452
+    public static void main(String[] args) {
+        PesawatTelepon pt = new PesawatTelepon();
+
+        pt.setTombol("untuk memasukan nomer");
+        System.out.println("pencet tombolnya " + pt.getTombol());
+    }
+}
